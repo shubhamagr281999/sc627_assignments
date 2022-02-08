21649fda1bc729ff1e91489f52b9afb32bd86036
Shubham Agrawal <shubhamagr281999@gmail.com>
2022-02-08 15:28:26 +0530

    algo is bug free and works well with ROS also

--- assignment1/bug_1.py
+++ assignment1/bug_1.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import rospy
-from math import sqrt
+import actionlib
+from math import sqrt,atan2
 import numpy as np
 from helper import *
-from sc627_helper.msg import MoveXYActionGoal,MoveXYActionResult
-from time import sleep
+from sc627_helper.msg import MoveXYAction,MoveXYGoal, MoveXYResult
+from time import sleep,time
+import matplotlib.pyplot as plt
 class bugAlgorithm:
 	def __init__ (self):
 		self.start=[]
@@ -13,6 +15,14 @@ class bugAlgorithm:
 		self.obstacles=[]
 		self.rate=rospy.Rate(1)
 
+		#Initialize client
+		self.client = actionlib.SimpleActionClient('move_xy', MoveXYAction)
+		self.client.wait_for_server()
+		self.result=MoveXYResult()
+		self.result.pose_final.x=0
+		self.result.pose_final.y=0
+		self.theta=0
+
 		#reading input file, assuming that the polygon vertices are in anticlockwise direction
 		self.ReadFile()
 		# print(computeDistancePointToPolygon([2.91,2.35],self.obstacles[1]))
@@ -20,11 +30,12 @@ class bugAlgorithm:
 		self.f=open('output_1.txt','w')
 		self.f.write('{:.5f} , {:.5f} \n'.format(self.start[0],self.start[1]))
 
-		#publisher
-		self.goal_pub=rospy.Publisher('/move_xy/goal',MoveXYActionGoal,queue_size=1)
+		#plotting
+		self.waypoints=[[self.start[0]],[self.start[1]]]
+		self.distance_to_goal=[self.distance(self.start,self.goal)]
+		self.time_elapsed=[0]
+		self.start_time=0
 
-		#subscriber
-		self.odom_subscriber=rospy.Subscriber('/move_xy/result',MoveXYActionResult,self.odom_callback)
 
 	def ReadFile(self):
 		with open('input.txt','r') as f:
@@ -59,9 +70,7 @@ class bugAlgorithm:
 		# print(self.step_size)
 		# print(self.start)
 
-	def odom_callback(self,msg):
-		self.start=[msg.result.pose_final.x,msg.result.pose_final.y]
-		print('curent pose:',self.start)
+
 	def distance(self,a,b):
 		# print(sqrt((a[0]-b[0])**2+(a[1]-b[1])**2))
 		return sqrt((a[0]-b[0])**2+(a[1]-b[1])**2)
@@ -78,20 +87,30 @@ class bugAlgorithm:
 		goal_direction=[self.goal[0]-self.start[0],self.goal[1]-self.start[1]]
 		norm=sqrt(goal_direction[0]**2 + goal_direction[1]**2)
 		self.next_pose=[self.start[0]+goal_direction[0]*self.step_size/norm, self.start[1]+goal_direction[1]*self.step_size/norm]
-		print(self.next_pose)
-		self.moveToNextStep()
+		self.moveToNextStep(atan2(goal_direction[1],goal_direction[0]))
 
-	def moveToNextStep(self):
-		msg=MoveXYActionGoal()
-		msg.goal.pose_dest.x=self.next_pose[0]
-		msg.goal.pose_dest.y=self.next_pose[1]
-		self.f.write('{:.5f} , {:.5f} \n'.format(self.next_pose[0],self.next_pose[1]))
-		# self.goal_pub.publish(msg)
-		self.start=self.next_pose
-
+	def moveToNextStep(self,tangent):
+		msg=MoveXYGoal()
+		msg.pose_dest.x=self.next_pose[0]
+		msg.pose_dest.y=self.next_pose[1]
+		msg.pose_dest.theta=tangent
+		# self.result=self.client.get_result()
+		# print(self.next_pose[0],self.next_pose[1],tangent)
+		self.client.send_goal(msg)
+		self.client.wait_for_result()
+		self.result=self.client.get_result()
+		self.theta=self.result.pose_final.theta
+		self.start=[self.result.pose_final.x,self.result.pose_final.y]
+		self.f.write('{:.5f} , {:.5f} \n'.format(self.start[0],self.start[1]))
+		# self.start=self.next_pose
+		self.distance_to_goal.append(self.distance(self.start,self.goal))
+		self.time_elapsed.append(time()-self.start_time)
+		self.waypoints[0].append(self.start[0])
+		self.waypoints[1].append(self.start[1])
 
 	def bug_base(self):
 		print('Starting BugBase Algo to move to the goal')
+		self.start_time=time()
 		while (self.distance(self.start,self.goal)>self.step_size):
 			hindrance,tangent,obstacleNum = self.obstaclesCheck()			
 			if(hindrance):
@@ -99,13 +118,14 @@ class bugAlgorithm:
 				return -1
 			else:
 				self.moveOneStepTogoal()
-			sleep(0.1)
+			# sleep(0.1)
 
 		print('Goal reached :)')
 		return 1
 
 	def bug_1(self):
 		print('Starting Bug1 Algo to move to the goal')
+		self.start_time=time()
 		while (self.distance(self.start,self.goal)>self.step_size):
 			hindrance,tangent,obstacleNum = self.obstaclesCheck()			
 			if(hindrance):
@@ -117,7 +137,7 @@ class bugAlgorithm:
 				while(np.dot([phit[0]-pcurrent[0],phit[1]-pcurrent[1]],tangent)<=0 or self.distance(phit,pcurrent)>self.step_size):
 					#need to go around the obstacle
 					self.next_pose=[self.start[0]+tangent[0]*self.step_size,self.start[1]+tangent[1]*self.step_size] #moving along the obstacle
-					self.moveToNextStep()
+					self.moveToNextStep(atan2(tangent[1],tangent[0]))
 					tangent=computeTangentVectorToPolygon(self.start,self.obstacles[obstacleNum])
 					# print(self.next_pose,tangent)
 					# sleep(3)
@@ -132,7 +152,7 @@ class bugAlgorithm:
 				pcurrent=computeClosetPointToPolygon(self.start,self.obstacles[obstacleNum])
 				while(self.distance(pRelase,pcurrent)>self.step_size):
 					self.next_pose=[self.start[0]+tangent[0]*self.step_size,self.start[1]+tangent[1]*self.step_size] #moving along the obstacle
-					self.moveToNextStep()
+					self.moveToNextStep(atan2(tangent[1],tangent[0]))
 					tangent=computeTangentVectorToPolygon(self.start,self.obstacles[obstacleNum])
 					pcurrent=computeClosetPointToPolygon(self.start,self.obstacles[obstacleNum])				
 				print('clered a obstacle')
@@ -145,6 +165,7 @@ class bugAlgorithm:
 	def bug_0(self):
 		print('Starting Bug0 Algo to move to the goal')
 		goal_visibility=0
+		self.start_time=time()
 		while (self.distance(self.start,self.goal)>self.step_size):
 			hindrance,tangent,obstacleNum = self.obstaclesCheck()			
 			if(hindrance):
@@ -152,17 +173,14 @@ class bugAlgorithm:
 				# print(goal_visibility,tangent)
 				while (goal_visibility<0):					
 					self.next_pose=[self.start[0]+tangent[0]*self.step_size,self.start[1]+tangent[1]*self.step_size] #moving along the obstacle
-					self.moveToNextStep()
-					# print(self.start,obstacleNum,tangent)
-					
+					self.moveToNextStep(atan2(tangent[1],tangent[0]))
+					# print(self.start,obstacleNum,tangent)					
 					tangent=computeTangentVectorToPolygon(self.start,self.obstacles[obstacleNum])
 					goal_visibility=np.cross([self.goal[0]-self.start[0],self.goal[1]-self.start[1]],tangent)
-					sleep(0.1)
 				print('cleared a obstacles. The gaol is visible again.')
 			
 			if(goal_visibility>=0 and (not hindrance)):
 				self.moveOneStepTogoal()
-			sleep(0.1)
 
 		print('Goal reached :)')
 		return 1
@@ -174,6 +192,18 @@ if __name__ == '__main__':
 	bugSolver.bug_1()
 	bugSolver.f.close()
 	rospy.loginfo('The way points are saved in output.txt. Closing the node')
+	plt.plot(bugSolver.waypoints[0],bugSolver.waypoints[1])
+	plt.title('Path taken by the bot')
+	plt.xlabel('X')
+	plt.ylabel('Y')
+	plt.show()
+	plt.plot(bugSolver.time_elapsed,bugSolver.distance_to_goal)
+	plt.title('Time vs distance_to_goal')
+	plt.ylabel('distance_to_goal')
+	plt.xlabel('time in seconds	')
+	plt.show()
+
+
 
 
 
